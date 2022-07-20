1ebaae48af012321fe4eb231bcd4390624cee593
Blackharddd <djalexmurcer@gmail.com>
2022-07-21 00:14:39 +0300

    -added currency support for administrator notification;

--- templates/emails/booking/cart.php
+++ templates/emails/booking/cart.php
@@ -1,3 +1,9 @@
+<?php
+
+$currency = carbon_get_theme_option( 'currency_' . pll_default_language() );
+
+?>
+
 <tr>
 	<td>
 		<table width="100%" style="border-spacing: 0">
@@ -7,11 +13,11 @@
 			<?php foreach( $data['services'] as $service ) : ?>
 				<tr>
 					<td><?=$service['name']; ?></td>
-					<td><?=$service['price']; ?> грн</td>
+					<td><?=$service['price'] . ' ' . $currency; ?></td>
 				</tr>
 			<?php endforeach; ?>
 			<tr>
-                <td colspan="2" style="padding-top: 10px;"><b><?=__( 'Итого', 'pdp_core' ); ?>:</b> <?=$data['total']; ?> грн</td>
+                <td colspan="2" style="padding-top: 10px;"><b><?=__( 'Итого', 'pdp_core' ); ?>:</b> <?=$data['total'] . ' ' . $currency; ?></td>
 			</tr>
 		</table>
 	</td>
